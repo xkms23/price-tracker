c0831accda3a19b66882c605d2608585edabcef4
Sebastian Janiszek <xkms23@o2.pl>
2025-12-09 17:09:35 +0100

    Added XKOM parser

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 from bs4 import BeautifulSoup
 import requests
-
+import cloudscraper
 
 class BaseScraper:
     def __init__(self):
         self.HEADERS = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0',
-            'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3'
+            'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3',
+            'Accept' : 'application/x-clarify-gzip',
+            #'Accept-Encoding' : 'gzip, deflate, br'
         }
+        self.adv = False
 
     def get_html(self, url):
-        return requests.get(url, headers=self.HEADERS).text
+        r = requests.post(url, headers=self.HEADERS)
+        return r.text
 
-    def get_soup(self, url):
+    def get_html_advanced(self, url):
+        r = cloudscraper.create_scraper(delay=10).get(url)
+        return r.text
+
+    def get_soup(self, url, adv):
+
+        if adv:
+
+        test = BeautifulSoup(self.get_html(url), 'html.parser')
+        if test.title.text == 'Just a moment...':
+            return BeautifulSoup(self.get_html_advanced(url), 'html.parser')
         return BeautifulSoup(self.get_html(url), 'html.parser')
 
     def get_price(self):
         pass
 
 class AmazonScraper(BaseScraper):
-    def __init__(self, url):
+    def __init__(self, url, adv):
         super().__init__()
         self.url = url
+        self.adv = adv
 
     def get_price(self):
         soup = self.get_soup(self.url)
@@ -29,10 +44,22 @@ class AmazonScraper(BaseScraper):
         price_fraction = soup.find('span', 'a-price-fraction').get_text()
         return float(f'{price_whole}.{price_fraction}')
 
+class XKomScraper(BaseScraper):
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+
+    def get_price(self):
+        soup = self.get_soup(self.url)
+        print(soup.prettify())
+        price = float(soup.find('span', 'parts__ScreenReaderPrice-sc').get_text()[6:15].replace(' ', '').replace(',', '.'))
+        return price
+
 class App:
     def __init__(self):
         self.available_sites = {
-            "www.amazon.": (AmazonScraper, [])
+            "www.amazon.": (AmazonScraper, []),
+            "www.x-kom.pl" : (XKomScraper, [])
         }
         self.start()
 
