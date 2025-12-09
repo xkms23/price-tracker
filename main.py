ba0636108ab673da01edd047b23d2a40e00a91c2
Sebastian Janiszek <xkms23@o2.pl>
2025-12-09 14:35:45 +0100

    Changed requests library and added HEADERS

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from bs4 import BeautifulSoup
-import urllib.request
+# import urllib.request
+import requests
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0',
+    'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3'
+} 
 
 def get_html(url_):
-    fp = urllib.request.urlopen(url_)
-    mbytes = fp.read()
-    html = mbytes.decode("utf-8")
-    fp.close()
-    return html
+    response = requests.get(url_, headers=HEADERS)
+    text = response.text
+    return text
+
+# Implementation using urllib.request
+
+# def get_html(url_):
+#     fp = urllib.request.urlopen(url_)
+#     mbytes = fp.read()
+#     html = mbytes.decode("utf-8")
+#     fp.close()
+#     return html
 
 def get_soup(url_):
     soup = get_html(url_)
@@ -30,3 +43,6 @@ for k, v in available_sites.items():
         func, arg =  v
         price = func(url)
         print(price)
+
+
+
